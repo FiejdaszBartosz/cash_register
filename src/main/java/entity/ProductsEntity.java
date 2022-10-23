9aaa694ff4d1ee4e72a7812b698c2be41a52c242
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2022-10-23 22:08:11 +0200

    Added database

---
+++ src/main/java/entity/ProductsEntity.java
@@ -0,0 +1,67 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products", schema = "cash_register", catalog = "")
+public class ProductsEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "product_name")
+    private String productName;
+    @Basic
+    @Column(name = "price")
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProductsEntity that = (ProductsEntity) o;
+
+        if (id != that.id) return false;
+        if (Double.compare(that.price, price) != 0) return false;
+        if (productName != null ? !productName.equals(that.productName) : that.productName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = id;
+        result = 31 * result + (productName != null ? productName.hashCode() : 0);
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
