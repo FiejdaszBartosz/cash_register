9e82abfad262b01e7b208df7d21634bf19e9e369
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2022-10-26 14:48:10 +0200

    Added ProductManager.deleteProductById

--- src/main/java/entity/ProductsEntity.java
+++ src/main/java/entity/ProductsEntity.java
@@ -5,30 +5,31 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "products", schema = "cash_register")
 
-@NamedQuery(name = "ProductsEntity.ById", query = "select p from  ProductsEntity p where p.idProducts = ?1")
+@NamedQuery(name = "ProductsEntity.ById", query = "select p from  ProductsEntity p where p.productId = ?1")
 @NamedQuery(name = "ProductsEntity.ByName", query = "select p from  ProductsEntity p where p.productName = ?1")
-@NamedQuery(name = "DeleteProduct", query = "delete from ProductsEntity p where p.idProducts = ?1")
+@NamedQuery(name = "DeleteProduct", query = "delete from ProductsEntity p where p.productId = ?1")
 
 public class ProductsEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    @Column(name = "id_products")
-    private int idProducts;
+    @Column(name = "product_id")
+    private int productId;
     @Basic
     @Column(name = "product_name")
     private String productName;
     @Basic
     @Column(name = "price")
     private double price;
-    @OneToOne(mappedBy = "productsByIdMagazineProducts")
-    private MagazineEntity magazineByIdProducts;
+    @Basic
+    @Column(name = "amount")
+    private int amount;
 
-    public int getIdProducts() {
-        return idProducts;
+    public int getProductId() {
+        return productId;
     }
 
-    public void setIdProducts(int idProducts) {
-        this.idProducts = idProducts;
+    public void setProductId(int productId) {
+        this.productId = productId;
     }
 
     public String getProductName() {
@@ -47,6 +48,14 @@ public class ProductsEntity {
         this.price = price;
     }
 
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -54,8 +63,9 @@ public class ProductsEntity {
 
         ProductsEntity that = (ProductsEntity) o;
 
-        if (idProducts != that.idProducts) return false;
+        if (productId != that.productId) return false;
         if (Double.compare(that.price, price) != 0) return false;
+        if (amount != that.amount) return false;
         if (productName != null ? !productName.equals(that.productName) : that.productName != null) return false;
 
         return true;
@@ -65,18 +75,11 @@ public class ProductsEntity {
     public int hashCode() {
         int result;
         long temp;
-        result = idProducts;
+        result = productId;
         result = 31 * result + (productName != null ? productName.hashCode() : 0);
         temp = Double.doubleToLongBits(price);
         result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + amount;
         return result;
-    }
-
-    public MagazineEntity getMagazineByIdProducts() {
-        return magazineByIdProducts;
-    }
-
-    public void setMagazineByIdProducts(MagazineEntity magazineByIdProducts) {
-        this.magazineByIdProducts = magazineByIdProducts;
     }
 }
--- src/main/java/entity/entityManager/ProductManager.java
+++ src/main/java/entity/entityManager/ProductManager.java
@@ -1,9 +1,7 @@
 package entity.entityManager;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import entity.ProductsEntity;
+import jakarta.persistence.*;
 
 public class ProductManager {
     /**
@@ -13,11 +11,62 @@ public class ProductManager {
      * @param amount amount of new product
      */
     public static void createProduct(String name, double price, int amount) {
+        TypedQuery<ProductsEntity> productByName;
         int check;
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction entityTransaction = entityManager.getTransaction();
 
+        productByName = entityManager.createNamedQuery("ProductsEntity.ByName", ProductsEntity.class);
+        productByName.setParameter(1, name);
+        check = productByName.getResultList().size();
 
+        if (check == 0) {
+            try {
+                entityTransaction.begin();
+
+                ProductsEntity newProduct = new ProductsEntity();
+                newProduct.setProductName(name);
+                newProduct.setPrice(price);
+                newProduct.setAmount(amount);
+
+                entityManager.persist(newProduct);
+                entityTransaction.commit();
+            } finally {
+                if (entityTransaction.isActive()) entityTransaction.rollback();
+                entityManager.close();
+                entityManagerFactory.close();
+            }
+        }
     }
+
+    public static void deleteProductById(int id) {
+        TypedQuery<ProductsEntity> productById;
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        productById = entityManager.createNamedQuery("ProductsEntity.ById", ProductsEntity.class);
+        productById.setParameter(1, id);
+        ProductsEntity productsEntity = productById.getSingleResult();
+
+        if (productsEntity != null) {
+            try {
+                entityTransaction.begin();
+
+                entityManager.remove(productsEntity);
+
+                entityManager.flush();
+                entityManager.clear();
+                entityTransaction.commit();
+            } finally {
+                if (entityTransaction.isActive()) entityTransaction.rollback();
+                entityManager.close();
+                entityManagerFactory.close();
+            }
+        }
+    }
+
+
+
 }
